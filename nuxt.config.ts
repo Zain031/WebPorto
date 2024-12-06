2972ce87c4a4431294e145b3f23126181ec74ea4
ZAINU FIRDAUS IMANSYAH <zazain031@gmail.com>
2024-12-06 14:46:36 +0700

    fix:config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,28 @@
 export default defineNuxtConfig({
-    nitro: {
+  ssr: false,
+
+  nitro: {
+      preset: 'node-server',
       output: {
-        exclude: [
-          'node_modules/**/*', // Exclude node_modules
-          'test/**/*',         // Exclude test files
-          'docs/**/*',         // Exclude documentation
-          'examples/**/*',     // Exclude examples
-        ],
+          exclude: [
+              "node_modules/**/*", // Exclude node_modules
+              "test/**/*", // Exclude test files
+              "docs/**/*", // Exclude documentation
+              "examples/**/*", // Exclude examples
+          ],
       },
-    },
-    css: [
-      "swiper/css/bundle",             // Swiper styles
-      "@/assets/fonts/remixicon.css",  // Remixicon font styles
+  },
+
+  css: [
+      "swiper/css/bundle", // Swiper styles
+      "@/assets/fonts/remixicon.css", // Remixicon font styles
       "@/assets/css/nice-select.min.css", // Nice-select styles
-      "@/assets/css/animate.min.css",  // Animation styles
-      "@/assets/css/spacing.css",      // Spacing styles
+      "@/assets/css/animate.min.css", // Animation styles
+      "@/assets/css/spacing.css", // Spacing styles
       "bootstrap/dist/css/bootstrap.min.css", // Bootstrap styles
-      "@/assets/css/style.css",        // Main stylesheet
-      "@/assets/css/responsive.css",   // Responsive styles
-    ],
-  });
+      "@/assets/css/style.css", // Main stylesheet
+      "@/assets/css/responsive.css", // Responsive styles
+  ],
+
+  compatibilityDate: "2024-12-06",
+});
